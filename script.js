96c951c7ed62cf8d1a2a6790fd407fa7b5f49440
Shiya Negi <141110102+ShiyaNegi@users.noreply.github.com>
2024-08-12 15:28:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('DOM fully loaded and parsed');
+    // Add any JavaScript you want to run after the DOM is fully loaded here
+});
